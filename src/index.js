a1148eb3c12ce68b9d3ca557ae495bacf142958f
Natalia Sabadysh <natalia@Tests-MacBook-Pro.local>
2023-09-06 15:03:51 -0700

    server has started at http://localhost:4000/graphql/

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('Stated')
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const schema = gql`
+  type Query {
+    hello: String 
+  }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello Client'
+    }
+}
+
+const PORT = 4000;
+
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers
+})
+
+
+// midleware 
+server.applyMiddleware({ app })
+
+// run the server 
+app.listen ({ port: PORT}, ()=> console.log(`server ready at http://localhost:${PORT}${server.graphqlPath}/`))
